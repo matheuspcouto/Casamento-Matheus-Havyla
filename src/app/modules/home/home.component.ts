a832dbd112ac32d475dca230819cf57ab7b44593
Matheus <matheuspcouto70@gmail.com>
2023-02-15 10:53:42 -0300

    Colocando timer

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
-import { Event, Router, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {
+  Event,
+  Router,
+  NavigationStart,
+  NavigationEnd,
+  NavigationError,
+} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
   loading = false;
   disabled: boolean = false;
+  hours: number = 0;
+  minutes: number = 0;
+  seconds: number = 0;
+  days: number = 0;
+  animate: boolean = false;
+  timer: any;
+  dataCasamento = new Date('2023-11-24T19:00:00.817Z');
 
-  constructor(private router: Router){
+  constructor(private router: Router) {
     this.router.events.subscribe((event: Event) => {
       if (event instanceof NavigationStart) {
         this.loading = true;
@@ -19,6 +31,40 @@ export class HomeComponent {
       if (event instanceof NavigationEnd || event instanceof NavigationError) {
         this.loading = false;
       }
-    })
+    });
+  }
+
+  ngOnInit() {
+    this.start();
+  }
+
+  updataCasamentoTimer() {
+    let dataAtual = new Date()
+
+    var timeDiff = Math.abs(this.dataCasamento.getTime() - dataAtual.getTime());
+    var resultadoDias = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+    this.days =  resultadoDias;
+    this.hours = this.dataCasamento.getHours() - dataAtual.getHours();
+    this.minutes = this.dataCasamento.getMinutes() - dataAtual.getMinutes();
+    this.seconds = this.dataCasamento.getSeconds();
+
+    const time = this.dataCasamento.getTime();
+    this.dataCasamento.setTime(time - 1000); //---
+
+    /*     if (
+      this.dataCasamento.getHours() === 0 &&
+      this.dataCasamento.getMinutes() === 0 &&
+      this.dataCasamento.getSeconds() === 0
+    ) {
+    } */
+  }
+
+  start() {
+    this.updataCasamentoTimer();
+
+    this.timer = setInterval(() => {
+      this.updataCasamentoTimer();
+    }, 1000);
   }
 }
