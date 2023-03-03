f68223490ced83dd702bbfed5426fbaea4147a84
Matheus <matheuspcouto70@gmail.com>
2023-03-03 14:49:29 -0300

    Logo e ajustes

--- src/app/services/lista-presentes.service.ts
+++ src/app/services/lista-presentes.service.ts
@@ -8,18 +8,10 @@ export class ListaPresentesService {
 
   constructor(private httpClient: HttpClient) { }
 
-  //private encodeAuth = btoa(ApiInfo.LOGIN + ':' + ApiInfo.PASSWORD);
-  //private authorization = `Basic ${this.encodeAuth}`;
-
-  //private headers = new HttpHeaders({'Content-Type': 'application/json;charset=UTF-8', 'charset': 'UTF-8', Authorization: this.authorization });
   private headers = new HttpHeaders({'Content-Type': 'text/plain;charset=UTF-8'});
 
   getItens(): Observable<any> {
     return this.httpClient.get<any>(`${ApiInfo.HOST}?method=itens`, { headers: this.headers });
-  }
-
-  getItemById(id: any): Observable<any> {
-    return this.httpClient.get<any>(`${ApiInfo.HOST}?method=item&id=${id}`, { headers: this.headers });
   }
 
   salvarItem(body: any, id: any): Observable<any> {
